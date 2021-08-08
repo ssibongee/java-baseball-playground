04f7acc45ad9809c2f84af7a9704aa4f93a3cc26
ssibongee <ssibongee@gmail.com>
2021-08-09 03:25:43 +0900

    feat: 게임 실행 결과를 화면에 출력할 BillBoard 클래스 구현

---
+++ src/main/java/baseball/BillBoard.java
@@ -0,0 +1,20 @@
+package baseball;
+
+public class BillBoard {
+
+    public static void start() {
+        System.out.println("START");
+    }
+
+    public static void showResult(PlayResult result) {
+        System.out.println(result);
+    }
+
+    public static void success() {
+        System.out.println("YOU WIN");
+    }
+
+    public static void end() {
+        System.out.println("END");
+    }
+}
