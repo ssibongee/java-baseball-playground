632d7604b93086158d2161b4110f36f6ca261652
ssibongee <ssibongee@gmail.com>
2021-08-09 03:26:10 +0900

    feat: 게임을 실행하는 메인 클래스 작성

---
+++ src/main/java/baseball/BaseBallGameApplication.java
@@ -0,0 +1,9 @@
+package baseball;
+
+public class BaseBallGameApplication {
+
+    public static void main(String[] args) {
+        GameManager manager = new GameManager();
+        manager.start();
+    }
+}
